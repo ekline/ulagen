e433088f7614dbb9286567a43b92dde7d50d0283
Erik Kline <ekline@gmail.com>
2020-07-11 17:36:18 -0700

    Make random bytes injectable.
    This will make a unit test possible in future.

--- ulagen.c
+++ ulagen.c
@@ -31,27 +31,36 @@ SOFTWARE.
 #include <stdlib.h>
 #include <string.h>
 
-int make_random_ula_prefix(struct in6_addr *in6) {
+int make_ula_prefix(size_t (*rand_bytes)(uint8_t*, int num_bytes),
+                    struct in6_addr *in6) {
     struct in6_addr ip6;
 
-    if (in6 == NULL) {
+    if ((rand_bytes == NULL) || (in6 == NULL)) {
         return -1;
     }
 
     memset(ip6.s6_addr, 0, sizeof(ip6.s6_addr));
     ip6.s6_addr[0] = 0xfd;
 
-    FILE* fp = fopen("/dev/urandom", "r");
-    if (fp == NULL) {
-        return -2;
-    }
-
-    const size_t read = fread(&(ip6.s6_addr[1]), 1, 5, fp);
-    fclose(fp);
-    if (read == 5) {
+    if (rand_bytes(&(ip6.s6_addr[1]), 5) == 5) {
         memcpy(in6->s6_addr, ip6.s6_addr, sizeof(ip6.s6_addr));
         return 0;
     }
 
     return -1;
+}
+
+static size_t dev_urandom_bytes(uint8_t* dst, int num_bytes) {
+    FILE* fp = fopen("/dev/urandom", "r");
+    if (fp == NULL) {
+        return 0;
+    }
+
+    const size_t read = fread(dst, 1, num_bytes, fp);
+    fclose(fp);
+    return read;
+}
+
+int make_random_ula_prefix(struct in6_addr *in6) {
+    return make_ula_prefix(&dev_urandom_bytes, in6);
 }
--- ulagen.h
+++ ulagen.h
@@ -29,6 +29,22 @@ SOFTWARE.
 
 #include <netinet/in.h>
 
+/**
+ * Generates a ULA /48 prefix in fd00::/8 using the injected rand_bytes
+ * method to fill in the requisite 40 bits with random values.
+ *
+ * Returns 0 on success, non-zero otherwise.
+ */
+int make_ula_prefix(size_t (*rand_bytes)(uint8_t*, int num_bytes),
+                    struct in6_addr *in6);
+
+
+/**
+ * Generates a ULA /48 prefix in fd00::/8 using an injected function
+ * that fills in the requisite 40 bits with values from /dev/urandom.
+ *
+ * Returns 0 on success, non-zero otherwise.
+ */
 int make_random_ula_prefix(struct in6_addr *in6);
 
 #endif   /* ULAGEN_H__ */
